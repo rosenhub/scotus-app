4faedf4a00f9a6723673fb39dafecd60bff45d36
Josh <josh3161@gmail.com>
2018-03-03 15:52:29 -0600

    fixed 4th tab
    need to remove folders now

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -7,6 +7,6 @@ export const MainPage = 'TabsPage';
 
 // The initial root pages for our tabs (remove if not using tabs)
 export const Tab1Root = 'ListMasterPage';
-export const Tab2Root = 'SearchPage';
-export const Tab3Root = 'SettingsPage';
-export const Tab4Root = 'SearchPage';
+export const Tab2Root = 'CardsPage';
+export const Tab3Root = 'ListMasterPage';
+export const Tab4Root = 'CardsPage';
