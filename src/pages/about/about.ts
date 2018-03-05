fa37282b60bebf1d37781ddedc9f29b33d0bdc03
Josh <josh3161@gmail.com>
2018-03-04 21:36:44 -0600

    Admob Working!
    Works on Button from About
    Need to remove and put on tab.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
 
 /**
  * Generated class for the AboutPage page.
@@ -15,8 +16,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public admob: AdMobFree) {
   }
+
+  showBanner() {
+
+    let bannerConfig: AdMobFreeBannerConfig = {
+        // isTesting: no, // Remove in production
+        autoShow: true,
+        overlap: false,
+        bannerAtTop: true,
+        offsetTopBar: true,
+        id: 'ca-app-pub-6494883128831450/1590258392'
+    };
+
+    this.admob.banner.config(bannerConfig);
+
+    this.admob.banner.prepare().then(() => {
+     this.admob.banner.show()
+        // success
+    }).catch(e => console.log(e));
+
+}
+
+launchInterstitial() {
+
+    let interstitialConfig: AdMobFreeInterstitialConfig = {
+        // isTesting: true, // Remove in production
+        autoShow: true,
+        id: 'ca-app-pub-6494883128831450/1590258392'
+    };
+
+    this.admob.interstitial.config(interstitialConfig);
+
+    this.admob.interstitial.prepare().then(() => {
+        // success
+      }); 
+ } 
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
--- src/pages/timeline/timeline.module.ts
+++ src/pages/timeline/timeline.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TimelinePage } from './timeline';
+import  { VerticalTimeLine } from '../../components/verticaltimeline/verticaltimeline';
 
 @NgModule({
   declarations: [
     TimelinePage,
+    VerticalTimeLine
   ],
   imports: [
     IonicPageModule.forChild(TimelinePage),
