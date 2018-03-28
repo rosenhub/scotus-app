3a0a68632b0fd968b1ca770771493a344e0de3f4
Josh <josh3161@gmail.com>
2018-03-28 17:17:47 -0500

    Audio working in cards

--- src/pages/audio/audio.ts
+++ src/pages/audio/audio.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { NativeAudio } from '@ionic-native/native-audio';
 
 @IonicPage()
 @Component({
@@ -9,16 +10,18 @@ import { IonicPage, NavController } from 'ionic-angular';
 export class AudioPage {
   cardItems: any[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, nativeAudio: NativeAudio) {
     this.cardItems = [
       {
         user: {
-          avatar: 'assets/img/marty-avatar.png',
-          name: 'Marty McFly'
+          avatar: 'assets/img/justices/ruth_bader_ginsburg.jpg',
+          name: 'Justice Ginsburg'
         },
-        date: 'November 5, 1955',
-        image: 'assets/img/advance-card-bttf.png',
-        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+        date: 'November 5, 1997',
+        title: 'Smith v. State of Texas',
+        image: 'assets/img/justices/ruth_bader_ginsburg.jpg',
+        audio: 'assets/audio/april2016/15-375.mp3',
+        content: 'This landmark case was heard in 1997 and helped determine whether standing for the Pledge of Allegiance can be required in public schools. The decision was in favor of the Plaintiff 4-5 with Justice Ruth Bader Ginsburg writing the final interpretation.',
       },
       {
         user: {
@@ -26,7 +29,9 @@ export class AudioPage {
           name: 'Sarah Connor'
         },
         date: 'May 12, 1984',
+        title: 'Bob v. Bob',
         image: 'assets/img/advance-card-tmntr.jpg',
+        audio: 'assets/audio/april2016/15-420.mp3',
         content: 'I face the unknown future, with a sense of hope. Because if a machine, a Terminator, can learn the value of human life, maybe we can too.'
       },
       {
@@ -35,7 +40,9 @@ export class AudioPage {
           name: 'Dr. Ian Malcolm'
         },
         date: 'June 28, 1990',
+        title: 'M. Lewinsky v. W. Clinton',
         image: 'assets/img/advance-card-jp.jpg',
+        audio: 'assets/audio/april2016/15-446.mp3',
         content: 'Your scientists were so preoccupied with whether or not they could, that they didn\'t stop to think if they should.'
       }
     ];
